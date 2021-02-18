679c059a43c51376c2cebcbc25927d2acddc8507
ziakn <ziakn03@gmail.com>
2021-02-18 11:45:03 +0300

    Footer Fixed

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,10 @@
 <template>
-  <div>
+  <div class="flex flex-col h-screen">
     <Header />
-    <div class="container shadow-lg mx-auto bg-white md:mt-40 h-screen">
+    <div class="container shadow-lg mx-auto bg-white ">
       <Nuxt />
     </div>
+<Footer/>
   </div>
 </template>
 <script>
