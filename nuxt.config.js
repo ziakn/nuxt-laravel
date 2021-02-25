891630e004f047e00c0f625fc3f84e404e3db65a
ziakn <ziakn03@gmail.com>
2021-02-25 14:53:42 +0300

    Tailwind Removed

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,10 +14,16 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+     'quill/dist/quill.core.css',
+    // for snow theme
+    'quill/dist/quill.snow.css',
+    // for bubble theme
+    'quill/dist/quill.bubble.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+     { src: '~plugins/nuxt-quill-plugin', ssr: false }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -26,7 +32,6 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
